0b99563d0ef0d9632c11e70f52b7e5948f04ce38
Gyeonghun Park <gp112795@gmail.com>
2020-11-14 14:58:31 -0500

    Sign In part Two

--- screens/Auth/SignIn.js
+++ screens/Auth/SignIn.js
@@ -1,56 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { View, Text } from "react-native";
 import styled from "styled-components/native";
-import { StatusBar } from "react-native";
-import { BlurView } from "expo-blur";
 import Btn from "../../components/Auth/Btn";
-
-const LOGO_URL =
-  "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png";
+import Input from "../../components/Auth/Input";
 
 const Container = styled.View`
   flex: 1;
+  justify-content: center;
 `;
 
-const Image = styled.Image`
-  position: absolute;
-  z-index: -1;
-  top: 0;
-`;
+const TextInput = styled.TextInput``;
 
-const Logo = styled.Image`
-  margin-top: 50px;
-  width: 100px;
-  height: 100px;
-`;
-
-const BtnContainer = styled.View`
-  margin-top: 40px;
-`;
-
-export default ({ navigation }) => {
-  const goToSignUp = () => navigation.navigate("SignUp");
-  const goToSignIn = () => navigation.navigate("SignIn");
+export default () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => alert(`${username}${password}`);
   return (
     <Container>
-      <BlurView
-        intensity={40}
-        tint="light"
-        style={{
-          flex: 1,
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Logo source={{ uri: LOGO_URL }} />
-        <BtnContainer>
-          <Btn onPress={goToSignUp} text={"Sign Up"} accent={true} />
-
-          <Btn onPress={goToSignIn} text={"Sign In"} />
-        </BtnContainer>
-      </BlurView>
-      <Image source={require("../../assets/loginBg.jpeg")} />
-      <StatusBar barStyle="light-content" />
+      <Input
+        value={username}
+        placeholder="Username"
+        autoCapitalize="none"
+        stateFn={setUsername}
+      />
+      <Input
+        value={password}
+        placeholder="Password"
+        isPassword={true}
+        stateFn={setPassword}
+      />
+      <Btn text={"Sign In"} accent onPress={handleSubmit}></Btn>
     </Container>
   );
 };
