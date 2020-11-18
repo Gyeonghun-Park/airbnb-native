4115e13ce27ba0a26303eedb8d33922e9226b5ee
Gyeonghun Park <gp112795@gmail.com>
2020-11-18 10:35:27 -0500

    Fetching Rooms

--- screens/Main/Explore/index.js
+++ screens/Main/Explore/index.js
@@ -1,3 +1,11 @@
+import { connect } from "react-redux";
+import { getRooms } from "../../../redux/roomsSlice";
 import ExploreContainer from "./ExploreContainer";
 
-export default ExploreContainer;
+const mapDispatchToProps = (dispatch) => {
+  getRooms: dispatch(getRooms());
+};
+
+const mapStateToProps = (state) => state.roomsReducer.explore;
+
+export default connect(null, mapDispatchToProps)(ExploreContainer);
