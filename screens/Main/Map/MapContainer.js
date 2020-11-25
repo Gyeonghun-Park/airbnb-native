d9503df2bc7fa4de3404d86af85870d2acf81b32
Gyeonghun Park <gp112795@gmail.com>
2020-11-25 09:13:00 -0500

    Searching By Movement

--- screens/Main/Map/MapContainer.js
+++ screens/Main/Map/MapContainer.js
@@ -7,34 +7,46 @@ const { width, height } = Dimensions.get("screen");
 export default ({ rooms }) => {
   const mapRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
-  const onScroll = (e) => {
+  const onScroll = e => {
     const {
       nativeEvent: {
-        contentOffset: { x },
-      },
+        contentOffset: { x }
+      }
     } = e;
     const position = Math.abs(Math.round(x / width));
     setCurrentIndex(position);
   };
+  const moveMap = () => {
+    mapRef.current?.animateCamera(
+      {
+        center: {
+          latitude: parseFloat(rooms[currentIndex].lat),
+          longitude: parseFloat(rooms[currentIndex].lng)
+        }
+      },
+      { duration: 3000 }
+    );
+  };
   useEffect(() => {
     if (currentIndex !== 0) {
-      mapRef.current?.animateCamera(
-        {
-          center: {
-            latitude: parseFloat(rooms[currentIndex].lat),
-            longitude: parseFloat(rooms[currentIndex].lng),
-          },
-        },
-        { duration: 3000 }
-      );
+      moveMap();
     }
   }, [currentIndex]);
+  const onRegionChangeComplete = async () => {
+    try {
+      const { northEast, southWest } = await mapRef.current?.getMapBoundaries();
+      console.log(northEast, southWest);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
   return (
     <MapPresenter
       rooms={rooms}
       mapRef={mapRef}
       currentIndex={currentIndex}
       onScroll={onScroll}
+      onRegionChangeComplete={onRegionChangeComplete}
     />
   );
 };
